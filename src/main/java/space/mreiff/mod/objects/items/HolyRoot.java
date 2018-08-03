cfe4f953f2162fbf4e414184026042abc19e30d8
Mitchell Reiff <me@mreiff.space>
2018-08-03 00:41:46 -0400

    Holy Root

--- src/main/java/space/mreiff/mod/objects/items/HolyRoot.java
+++ src/main/java/space/mreiff/mod/objects/items/HolyRoot.java
@@ -8,6 +8,7 @@ import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import space.mreiff.mod.Main;
+import space.mreiff.mod.init.ItemInit;
 import space.mreiff.mod.util.interfaces.IHasModel;
 
 public class HolyRoot extends ItemFood implements IHasModel {
@@ -17,6 +18,9 @@ public class HolyRoot extends ItemFood implements IHasModel {
 		setUnlocalizedName(name);
 		setRegistryName(name);
 		setAlwaysEdible();
+		
+		ItemInit.ITEMS.add(this);
+
 
 	}
 
@@ -27,18 +31,18 @@ public class HolyRoot extends ItemFood implements IHasModel {
 
 	}
 	
-//	@Override
-//	protected void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) {
-////		player.addPotionEffect(new PotionEffect(Potion.jump.id, 900, 3));
-////		player.addPotionEffect(new PotionEffect(Potion.resistance.id, 900, 3));
-////		player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 900, 3));
-//		
-//		if(!worldIn.isRemote) {
-//			player.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 900, 3, false, true));
-//			player.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 900, 3, false, true));
-//			player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 900, 3, false, true));
-//
-//		}
-//	}
+	@Override
+	protected void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) {
+//		player.addPotionEffect(new PotionEffect(Potion.jump.id, 900, 3));
+//		player.addPotionEffect(new PotionEffect(Potion.resistance.id, 900, 3));
+//		player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 900, 3));
+		
+		if(!worldIn.isRemote) {
+			player.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 900, 3, false, true));
+			player.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 900, 3, false, true));
+			player.addPotionEffect(new PotionEffect(MobEffects.SPEED, 900, 3, false, true));
+
+		}
+	}
 
 }
